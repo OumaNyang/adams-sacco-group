8c6d6f7c485df7439da460fbe404bbab0ed01048
Milton Ouma <ouma.nyang1@gmail.com>
2022-08-06 15:23:14 +0300

    Navbar and Sidebar added

---
+++ src/component/App.js
@@ -0,0 +1,17 @@
+import "../App.css";
+import React from "react";
+import "./style.css";
+import SideNavBar from "./Sidebar";
+import HeaderBar from "./HeaderBar";
+
+function App() {
+  return (
+    <div>
+    <HeaderBar />
+
+    <SideNavBar />
+  </div>
+  );
+}
+
+export default App;
